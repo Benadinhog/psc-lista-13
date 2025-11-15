43f7c9aa05691ab0dd31fa7bb2f608c6362170b1
Bernardo Gonçalves <bernardolug316@gmail.com>
2025-11-15 12:03:38 -0300

    Add files via upload

---
+++ Funcionario.java
@@ -0,0 +1,23 @@
+public public class Funcionario extends Pessoa {
+
+    private double salario;
+    private String cargo;
+
+    public Funcionario(String nome, String cpf, int idade, double salario, String cargo) {
+        super(nome, cpf, idade);
+        this.salario = salario;
+        this.cargo = cargo;
+    }
+
+    public double calcularSalarioAnual() {
+        return salario * 12;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
---
+++ Gerente.java
@@ -0,0 +1,17 @@
+public class Gerente extends Funcionario {
+
+    private String departamento;
+
+    public Gerente(String nome, String cpf, int idade, double salario, String cargo, String departamento) {
+        super(nome, cpf, idade, salario, cargo);
+        this.departamento = departamento;
+    }
+
+    @Override
+    public void exibirDadosBasicos() {
+        super.exibirDadosBasicos();
+
+        System.out.println("Cargo: " + getCargo());
+        System.out.println("Departamento: " + departamento);
+    }
+}
